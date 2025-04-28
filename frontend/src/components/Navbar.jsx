4bf49295210ceede24dcf7e89b71b421a8b62c2b
Arbinniroul <arbinniroula21@gmail.com>
2025-04-28 21:18:28 +0545

    ScreenHome

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { LucideMenu, MenuIcon, Search } from 'lucide-react'
 const Navbar = ({handlePopUp}) => {
 
   return (
-    <div className='relative flex justify-between items-center py-3 px-4 sm:py-4 sm:px-6 md:px-10 lg:px-12 bg-black text-white'>
+    <div className='max-w-full  relative flex justify-between items-center py-3 px-4 sm:py-4 sm:px-6 md:px-10 lg:px-12 bg-black text-white'>
         <div className='flex items-center gap-10'>
         <h1 className='text-2xl tracking-tight uber'>Logo</h1>
             <div className='hidden lg:flex md:hidden text-sm font-semibold'>
@@ -17,7 +17,7 @@ const Navbar = ({handlePopUp}) => {
             </div>
         </div>
            <div className='flex items-center gap-3 lg:gap-1'>
-            <div className='md:hidden hidden lg:flex sm:hidden gap-2 px-4 py-1 items-center  justify-center bg-white rounded-full text-black'>
+            <div className='hidden lg:flex  gap-2 px-4 py-1 items-center  justify-center bg-white rounded-full text-black'>
               <Search className='size-4'  />
 
               <input type="text" className='outline-none border-none placeholder:text-sm px-1' placeholder='Search Uber.com '  />
@@ -27,7 +27,7 @@ const Navbar = ({handlePopUp}) => {
 
             </div>
             <div className='flex gap-3 text-sm font-medium items-center'>
-              <button className=' cursor-pointer hidden md:hidden lg:flex  px-3 py-2 rounded-full bg-black hover:bg-gray-800 transition-all transform duration-500 ease-in-out '>Help</button>
+              <button className=' cursor-pointer hidden  lg:flex  px-3 py-2 rounded-full bg-black hover:bg-gray-800 transition-all transform duration-500 ease-in-out '>Help</button>
               <button className='bg-white px-3 py-2 rounded-full text-black transition-all transform ease-in-out duration-300 cursor-pointer hover:bg-white/80' >My account</button>
               <button className='flex lg:hidden' onClick={()=>handlePopUp}><LucideMenu/></button>
             </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,37 +1,76 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { BatteryLow, Calendar, Calendar1, CalendarPlus, CalendarPlusIcon, Circle, Clock, Dot, DotSquare, DotSquareIcon, Send, Square, SquareDot } from 'lucide-react'
 
 const Home = () => {
   return (
-    <div className='min-h-screen'> 
+    <div className="min-w-[320px] w-full max-w-[100vw] overflow-x-hidden ">
       <Navbar/>
-       <div className='flex w-full px-15 py-20 gap-14 '>
-          <div className='w-1/2 flex flex-col gap-20 '>
-           <h1 className='text-6xl  font-semibold'>Go anywhere with Uber</h1>
+      <div className='flex flex-col lg:flex-row w-full px-4 sm:px-5 md:px-6 lg:px-14 py-10 sm:py-12 md:py-14 lg:py-15 gap-8 lg:gap-14'>
+
+          <div className='w-1/2 flex flex-col gap-30  '>
+          <div className='w-full'>
+          <h1 className='lg:text-6xl md:text-5xl  text-4xl w-full   font-semibold '>Go anywhere with Uber</h1>
+          </div>
           
-          <div className='flex flex-col gap-2  w-96'>
-            <input type="text" className='bg-gray-400' />
-            <input type="text" className='bg-gray-400' />
+          
+          <div className='flex flex-col gap-4 mx-auto md:mx-0 lg:w-[400px] w-[300px] justify-center relative'>
+            <div className='bg-[#f3f3f3] py-1 cursor-text w-full  px-4 gap-4 relative rounded group flex focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-black items-center justify-start'>
+
+           
+            <Dot className='size-2 stroke-2 bg-black rounded-full'/>
+          
+            
+          
+            
+            <input type="text" className='outline-none  py-2' placeholder='Pickup location' />
+
+            <Send className='absolute right-0 mr-3'/>
+            
+
+            </div>
+            <div className="flex justify-center absolute top-8 left-5 z-10">
+    <div className="border-l border-black h-10 w-0"></div>
+  </div>
+            <div className='bg-[#f3f3f3]  cursor-text px-4 gap-4 w-full py-1 relative rounded group flex focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-black items-center justify-start'>
+            
+            <Square className='size-2 stroke-2 bg-black'/>
+            <input type="text" className='outline-none  py-2' placeholder='Dropoff Location' />
+
+
+
+            </div>
+       
             <div className='flex gap-2'>
                           
-            <input type="text" className='w-1/2 bg-gray-400' /> 
-
-<input type="text" className='w-1/2 bg-gray-400' />
           
+              <div className='bg-[#f3f3f3] w-[50%]  cursor-text py-2 px-4 gap-3 rounded group flex focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-black items-center flex-1'>
+               <div>
+               <Calendar className='size-4 '/>
+               </div>
+                <input type="date" className='outline-none py-2 flex-1' placeholder='Today' />
+              </div>
+              
+              <div className='bg-[#f3f3f3] cursor-text w-1/2 py-2 px-4 gap-4 rounded group flex focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-black items-center flex-1'>
+                <div>
+                  <Clock className='size-4 '/>
+                </div>
+                <input type="text" className='outline-none py-2 flex-1' placeholder='Today' />
+              </div>
             </div>
+           
+
+          
+            
            
             <button></button>
             </div>
-
-
           </div>
-          <div >
-            <div className='w-full h-full bg-black '>
+          <div className="lg:w-1/2 w-full aspect-square">
+  <div className="w-full h-full bg-black rounded-lg">
 
-            hello
-            </div>
-
-          </div>
+  </div>
+</div>
 
        </div>
     </div>
