e92709f4f8e126c98ad6461c9a703870aad6d97a
Arbinniroul <arbinniroula21@gmail.com>
2025-04-29 08:29:36 +0545

    ResponsiveHomePage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -102,22 +102,22 @@ const Home = () => {
 
 
 
- <div className='min-h-screen px-7 py-12 md:px-17 md:py-20'>
+ <div className='min-h-screen px-8 py-20 lg:px-17'>
  <div className='grid grid-cols-1 lg:grid-cols-2 gap-26 '>
-  <div className='flex flex-col gap-5 md:py-10 justify-center '>
+  <div className='flex flex-col gap-5 md:py-10 justify-center order-1 lg:order-none '>
   <h1 className='text-4xl font-semibold'>Login to see your recent activity</h1>
   <p>View past trips, tailored suggestions, support resources, and more.</p>
   <button className='px-4 py-3 bg-black w-fit rounded-lg  hover:bg-[#000000]/86  text-sm font-semibold cursor-pointer text-white'>Login in to your account</button>
 
  <p className='border-b-1 border-black w-fit'>Dont have an Uber account ? SignUp</p>
   </div>
-  <div>
-    <img src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1730197725/assets/0f/48c7ba-da13-4fdc-b54c-42878042f513/original/Airport-Fall.png" alt="" />
+  <div className='order-2 lg:order-none' >
+    <img src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1730197725/assets/0f/48c7ba-da13-4fdc-b54c-42878042f513/original/Airport-Fall.png" className='w-full' alt="" />
   </div>
-  <div >
-    <img src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1684855112/assets/96/4dd3d1-94e7-481e-b28c-08d59353b9e0/original/earner-illustra.png" className='w-full' alt="" />
+  <div className='order-4 lg:order-none w-full' >
+    <img  src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1684855112/assets/96/4dd3d1-94e7-481e-b28c-08d59353b9e0/original/earner-illustra.png" className='w-full' alt="" />
   </div>
-  <div className='flex flex-col justify-center gap-5 md:py-10  '>
+  <div className='flex flex-col justify-center gap-5 md:py-10 order-3 lg:order-none  '>
   <h1 className='text-4xl font-semibold'>Drive when you want make what to need</h1>
   <p>Make money on your schedule with deliveries or rides—or both. You can use your own car or choose a rental through Uber.</p>
   <div className='flex gap-5 items-center '>
@@ -128,7 +128,7 @@ const Home = () => {
   </div>
  
   </div>
-  <div className='flex flex-col justify-center gap-5 md:py-10  '>
+  <div className='flex flex-col justify-center gap-5 md:py-10 order-5 lg:order-none '> 
   <h1 className='text-4xl font-semibold'>The Uber you know, reimagined for business</h1>
   <p>Uber for Business is a platform for managing global rides and meals, and local deliveries, for companies of any size</p>
   <button className='px-4 py-3 bg-black w-fit rounded-lg  hover:bg-[#000000]/86  text-sm font-semibold cursor-pointer text-white'>Get Started</button>
@@ -137,13 +137,13 @@ const Home = () => {
  </p>
 
   </div>
-  <div>
-    <img src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1684887108/assets/76/baf1ea-385a-408c-846b-59211086196c/original/u4b-square.png" alt="" />
+  <div className='order-6 lg:order-none' >
+    <img src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1684887108/assets/76/baf1ea-385a-408c-846b-59211086196c/original/u4b-square.png" className='w-full' alt="" />
   </div>
-  <div>
-    <img src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1696243800/assets/62/3b076a-3406-4f3b-89de-2cf1a2ccb907/original/uber-one.jpg" alt="" />
+  <div className='order-8 lg:order-none '>
+    <img src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_552,w_552/v1696243800/assets/62/3b076a-3406-4f3b-89de-2cf1a2ccb907/original/uber-one.jpg" className='w-full' alt="" />
   </div>
-  <div className='flex flex-col  gap-5 md:py-10 justify-center  '>
+  <div className='flex flex-col  gap-5 md:py-10 justify-center order-7 lg:order-none '> 
   <h1 className='text-4xl font-semibold'>Save on Uber and Uber Eats with Uber One membership</h1>
   <p className='mb-2'>Become an Uber One member for savings and exclusive perks on Uber and Uber Eats.</p>
   <p>Sign up for just $9.99/month.</p>
